192b97e0cd31639a594cafa70bbfe0cda2ae3757
kepha222 <kephamotari1@.com>
2022-10-09 02:02:06 +0300

    initial commit

---
+++ src/components/TextField.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { ErrorMessage, useField } from "formik";
+
+export const TextField = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  //   console.log(field, meta)
+  return (
+    <div className="mb-2">
+      <label htmlFor={field.name}>{label}</label>
+      <input
+        className={`form-control shadow-none ${
+          meta.touched && meta.error && "is-invalid"
+        }`}
+        {...field}
+        {...props}
+        autoComplete="off"
+      />
+      <ErrorMessage component="div" name={field.name} className="error" />
+    </div>
+  );
+};
+
+export default TextField;
